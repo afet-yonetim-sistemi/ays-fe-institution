29f1d7fb3e45d571175896e846be6f602f6caa22
Nurhak Keskin <77546954+sharp3614@users.noreply.github.com>
2024-05-20 13:52:30 +0300

    AYS-253 (#167)

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,5 +1,16 @@
+import LanguageToggle from '@/components/dashboard/languageToggle'
+import { ModeToggle } from '@/components/dashboard/modeToggle'
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return <div>{children}</div>
+  return (
+    <div>
+      <nav className="fixed flex gap-2 right-4 top-4">
+        <ModeToggle />
+        <LanguageToggle />
+      </nav>
+      {children}
+    </div>
+  )
 }
