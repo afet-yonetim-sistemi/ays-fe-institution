271a43498b3b1180a8985698b10512299b24a3ea
sharp3614 <knurhak09@gmail.com>
2024-06-04 11:14:08 +0300

    content deleted

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -2,10 +2,6 @@
 import PrivateRoute from '@/app/hocs/isAuth'
 
 const Page = () => {
-  return(
-      <PrivateRoute>
-      <div>Dashboard Page</div>
-  </PrivateRoute>
-  )
+  return <PrivateRoute></PrivateRoute>
 }
 export default Page
