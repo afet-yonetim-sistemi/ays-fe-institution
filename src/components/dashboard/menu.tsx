3d52ee9f6fa52235db43fd66f4c0b89af8b4eaf8
Buğra Ercan <bugrasshgercan@gmail.com>
2024-06-04 16:47:02 +0300

    AYS-256 added the version information to the sidebar menu

--- src/components/dashboard/menu.tsx
+++ src/components/dashboard/menu.tsx
@@ -2,13 +2,32 @@
 import clsx from 'clsx'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { MenuItems } from "@/constants/menuItems";
+import axios from 'axios';
+import http from '@/configs/axiosConfig'
   
 export default function Menu() {
   const pathname = usePathname()
   const { t } = useTranslation()
+
+  const [versionInfo, setVersionInfo] = useState("")
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await http.get('/public/actuator/info')
+        const version = response.data.application.version
+        setVersionInfo(version)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+    
+    fetchData()
+  }, [])
+
   return (
     <nav className="grid items-start gap-1 text-sm font-medium md:p-2 w-full">
       {MenuItems.map((item) => {
@@ -29,6 +48,11 @@ export default function Menu() {
           </Link>
         )
       })}
+      {versionInfo && (
+        <div className="text-center absolute bottom-2 text-xs text-gray-500 dark:text-gray-400">
+          {t("version")} {versionInfo}
+        </div>
+      )}
     </nav>
   )
 }
