9c6410a72c06b51f7d5d710ac22f9e900870c25e
Buğra Ercan <bugrasshgercan@gmail.com>
2024-06-05 23:22:10 +0300

    AYS-256 changed how we are getting the info, and showing it according to review notes

--- src/components/dashboard/menu.tsx
+++ src/components/dashboard/menu.tsx
@@ -4,55 +4,63 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { MenuItems } from "@/constants/menuItems";
-import axios from 'axios';
+import { MenuItems } from '@/constants/menuItems'
 import http from '@/configs/axiosConfig'
-  
+import packageInfo from '../../../package.json'
+
 export default function Menu() {
   const pathname = usePathname()
   const { t } = useTranslation()
 
-  const [versionInfo, setVersionInfo] = useState("")
+  const [versionInfo, setVersionInfo] = useState('')
+  const UIVersion = packageInfo.version
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await http.get('/public/actuator/info')
-        const version = response.data.application.version
-        setVersionInfo(version)
-      } catch (error) {
-        console.error('Error fetching data:', error)
-      }
+    const fetchData = () => {
+      http
+        .get('/public/actuator/info')
+        .then((response) => {
+          const APIVersion = response.data.application.version
+          setVersionInfo(`UI v${UIVersion} | API v${APIVersion}`)
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error)
+        })
     }
-    
+
     fetchData()
   }, [])
 
   return (
-    <nav className="grid items-start gap-1 text-sm font-medium md:p-2 w-full">
-      {MenuItems.map((item) => {
-        const LinkIcon = item.icon
-        return (
-          <Link
-            key={item.key}
-            href={item.key}
-            className={clsx(
-              'flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-colors hover:text-primary',
-              {
-                'bg-slate-200 text-black dark:bg-slate-600/50 dark:text-slate-50 font-semibold': pathname === item.key,
-              },
-            )}
-          >
-            <LinkIcon className="h-6 w-6" />
-            {t(item.label)}
-          </Link>
-        )
-      })}
+    <div className="flex flex-col h-full justify-between">
+      <nav className="grid items-start gap-1 text-sm font-medium md:p-2 w-full">
+        {MenuItems.map((item) => {
+          const LinkIcon = item.icon
+          return (
+            <Link
+              key={item.key}
+              href={item.key}
+              className={clsx(
+                'flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-colors hover:text-primary',
+                {
+                  'bg-slate-200 text-black dark:bg-slate-600/50 dark:text-slate-50 font-semibold':
+                    pathname === item.key,
+                },
+              )}
+            >
+              <LinkIcon className="h-6 w-6" />
+              {t(item.label)}
+            </Link>
+          )
+        })}
+      </nav>
       {versionInfo && (
-        <div className="text-center absolute bottom-2 text-xs text-gray-500 dark:text-gray-400">
-          {t("version")} {versionInfo}
+        <div className="flex justify-center py-2">
+          <span className="text-xs text-gray-500 dark:text-gray-400">
+            {versionInfo}
+          </span>
         </div>
       )}
-    </nav>
+    </div>
   )
 }
