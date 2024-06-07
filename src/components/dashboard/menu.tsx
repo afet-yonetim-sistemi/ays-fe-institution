00851d70b6ef7a23410aa6b1a6d6b32dec4b5307
Buğra Ercan <38170967+sanshigo345@users.noreply.github.com>
2024-06-07 22:54:33 +0300

    AYS-256 added the version information to the sidebar menu (#174)

--- src/components/dashboard/menu.tsx
+++ src/components/dashboard/menu.tsx
@@ -2,33 +2,63 @@
 import clsx from 'clsx'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { MenuItems } from "@/constants/menuItems";
-  
+import { MenuItems } from '@/constants/menuItems'
+import http from '@/configs/axiosConfig'
+import packageInfo from '../../../package.json'
+
 export default function Menu() {
   const pathname = usePathname()
   const { t } = useTranslation()
+
+  const UIVersion = packageInfo.version
+  const [versionInfo, setVersionInfo] = useState('')
+
+  useEffect(() => {
+    const fetchData = () => {
+      http
+        .get('/public/actuator/info')
+        .then((response) => {
+          const APIVersion = response.data.application.version
+          setVersionInfo(APIVersion)
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error)
+        })
+    }
+
+    fetchData()
+  }, [])
+
   return (
-    <nav className="grid items-start gap-1 text-sm font-medium md:p-2 w-full">
-      {MenuItems.map((item) => {
-        const LinkIcon = item.icon
-        return (
-          <Link
-            key={item.key}
-            href={item.key}
-            className={clsx(
-              'flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-colors hover:text-primary',
-              {
-                'bg-slate-200 text-black dark:bg-slate-600/50 dark:text-slate-50 font-semibold': pathname === item.key,
-              },
-            )}
-          >
-            <LinkIcon className="h-6 w-6" />
-            {t(item.label)}
-          </Link>
-        )
-      })}
-    </nav>
+    <div className="flex flex-col h-full justify-between">
+      <nav className="grid items-start gap-1 text-sm font-medium md:p-2 w-full">
+        {MenuItems.map((item) => {
+          const LinkIcon = item.icon
+          return (
+            <Link
+              key={item.key}
+              href={item.key}
+              className={clsx(
+                'flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-colors hover:text-primary',
+                {
+                  'bg-slate-200 text-black dark:bg-slate-600/50 dark:text-slate-50 font-semibold':
+                    pathname === item.key,
+                },
+              )}
+            >
+              <LinkIcon className="h-6 w-6" />
+              {t(item.label)}
+            </Link>
+          )
+        })}
+      </nav>
+      <div className="flex justify-center py-2">
+        <span className="text-xs text-gray-500 dark:text-gray-400">
+          UI {UIVersion} {versionInfo && `| API ${versionInfo}`}
+        </span>
+      </div>
+    </div>
   )
 }
