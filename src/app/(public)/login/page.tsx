467f4db9e485b41cce5248f4b5f15b3724eee610
kevserozkan <sumeyye.kevser.ozkan@finartz.com>
2024-06-07 14:59:50 +0300

    fix: change email input for hold the value

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -74,7 +74,7 @@ const Page = () => {
       })
       .catch((err) => {
         dispatch(loginFailed(err.message))
-        form.reset()
+        form.setValue('password', '')
       })
       .finally(() => setLoading(false))
   }
