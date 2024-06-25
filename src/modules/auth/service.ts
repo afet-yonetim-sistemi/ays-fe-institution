87a97a2a3c20fa17cd4f9c49391db7058039f002
Kevser Özkan <69302260+kevseroz@users.noreply.github.com>
2024-06-25 14:38:47 +0300

    AYS-281| Changed API versions and  API request for backend update. (#189)

--- src/modules/auth/service.ts
+++ src/modules/auth/service.ts
@@ -1,9 +1,9 @@
 import http, { api } from '@/configs/axiosConfig'
 
 const authService = {
-  login: (data: any) => api.post('/api/v2/authentication/token', data),
+  login: (data: any) => api.post('/api/v1/authentication/token', data),
   logout: (data: any) =>
-    http.post('/api/v2/authentication/token/invalidate', {
+    http.post('/api/v1/authentication/token/invalidate', {
       refreshToken: data,
     }),
 }
