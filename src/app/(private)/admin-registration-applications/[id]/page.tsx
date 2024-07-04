7de4e777e7cb73e118b9ae8c5a2d34f60ec5c7cf
Buğra Ercan <bugrasshgercan@gmail.com>
2024-07-04 22:36:46 +0300

    AYS-249: fix some css, good enough?

--- src/app/(private)/admin-registration-applications/[id]/page.tsx
+++ src/app/(private)/admin-registration-applications/[id]/page.tsx
@@ -1,22 +1,26 @@
-'use client';
+'use client'
 
-import { useEffect, useState } from "react";
-import { getAdminRegistrationApplication } from "@/modules/adminRegistrationApplications/service";
-import { Input } from "@/components/ui/input";
+import { useEffect, useState } from 'react'
+import { getAdminRegistrationApplication } from '@/modules/adminRegistrationApplications/service'
+import { Input } from '@/components/ui/input'
 import {
   Select,
   SelectTrigger,
   SelectValue,
   SelectContent,
   SelectItem,
-} from "@/components/ui/select";
-import { AdminRegistrationApplication } from "../types";
+} from '@/components/ui/select'
+import { AdminRegistrationApplication } from '../types'
+import { formatDate } from '@/app/hocs/formatDate'
+import Status from '@/modules/adminRegistrationApplications/components/status'
 
 const Page = ({ params }: { params: { slug: string; id: string } }) => {
-  const [adminRegistrationApplicationDetails, setAdminRegistrationApplicationDetails] =
-    useState<AdminRegistrationApplication | null>(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<any>(null);
+  const [
+    adminRegistrationApplicationDetails,
+    setAdminRegistrationApplicationDetails,
+  ] = useState<AdminRegistrationApplication | null>(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState<any>(null)
 
   useEffect(() => {
     const fetchDetails = () => {
@@ -25,85 +29,125 @@ const Page = ({ params }: { params: { slug: string; id: string } }) => {
           console.log(response)
           if (response.data.isSuccess) {
             console.log(response.data.response)
-            setAdminRegistrationApplicationDetails(response.data.response);
+            setAdminRegistrationApplicationDetails(response.data.response)
           } else {
-            setError(new Error("Failed to fetch data"));
+            setError(new Error('Failed to fetch data'))
           }
-          setLoading(false);
+          setLoading(false)
         })
         .catch((err) => {
-          setError(err);
-          setLoading(false);
-        });
-    };
-    fetchDetails();
-  }, [params.id]);
+          setError(err)
+          setLoading(false)
+        })
+    }
+    fetchDetails()
+  }, [params.id])
 
   const renderInput = (label: string, value: string | number | null) => (
     <div className="w-full">
-      <label className="block text-sm font-medium text-gray-700">{label}</label>
-      <Input value={value ?? ''} readOnly className="mt-1 block w-full" />
+      <label className="block text-sm font-medium text-gray-500">{label}</label>
+      <Input value={value ?? ''} disabled className="mt-1 block w-full" />
     </div>
-  );
+  )
 
   const renderPhoneInput = (countryCode: string, lineNumber: string) => {
     if (countryCode && lineNumber) {
       return (
         <div className="w-full">
-          <label className="block text-sm font-medium text-gray-700">Phone Number</label>
+          <label className="block text-sm font-medium text-gray-500">
+            Phone Number
+          </label>
           <Input
             value={`(+${countryCode}) ${lineNumber}`}
-            readOnly
-            className="mt-1 block w-full"
-          />
-        </div>
-      );
-    } else {
-      return (
-        <div className="w-full">
-          <label className="block text-sm font-medium text-gray-700">Phone Number</label>
-          <Input
-            value=""
-            readOnly
+            disabled
             className="mt-1 block w-full"
           />
         </div>
       )
+    } else {
+      return (
+        <div className="w-full">
+          <label className="block text-sm font-medium text-gray-500">
+            Phone Number
+          </label>
+          <Input value="" disabled className="mt-1 block w-full" />
+        </div>
+      )
     }
-  };
+  }
 
   return (
-    <div className="form-container p-6 bg-white rounded-md shadow-md">
+    <div className="form-container p-6 bg-white dark:bg-gray-800 rounded-md shadow-md text-black dark:text-white">
       {loading && <div>Loading...</div>}
       {error && <div>Error: {error}</div>}
       {!loading && !error && adminRegistrationApplicationDetails && (
         <form className="space-y-4">
           <div className="mb-6">
-            <h1 className="text-xl font-bold">
-              Institution ID: {adminRegistrationApplicationDetails.institution?.id}
-            </h1>
-            <p className="text-sm text-gray-700">
-              Created User: {adminRegistrationApplicationDetails.createdUser} | Created At: {adminRegistrationApplicationDetails.createdAt} | Status: {adminRegistrationApplicationDetails.status}
-            </p>
-          </div>
+  <h1 className="text-xl font-bold mb-1">
+    Institution ID: {adminRegistrationApplicationDetails.institution?.id}
+  </h1>
+  <div className="text-m text-gray-250 space-x-6">
+    <span>
+      Created User: {adminRegistrationApplicationDetails.createdUser}
+    </span>
+    <span>
+      Created At: {formatDate(adminRegistrationApplicationDetails.createdAt)}
+    </span>
+    <span>
+    Status: <Status status={adminRegistrationApplicationDetails.status} />
+    </span>
+  </div>
+</div>
           <div className="grid grid-cols-1 gap-y-6 sm:grid-cols-3 sm:gap-x-6">
-            {renderInput('Updated User', adminRegistrationApplicationDetails.updatedUser)}
-            {renderInput('Updated At', adminRegistrationApplicationDetails.updatedAt)}
+            {renderInput(
+              'Updated User',
+              adminRegistrationApplicationDetails.updatedUser,
+            )}
+            {renderInput(
+              'Updated At',
+              adminRegistrationApplicationDetails.updatedAt,
+            )}
             {renderInput('ID', adminRegistrationApplicationDetails.id)}
             {renderInput('Reason', adminRegistrationApplicationDetails.reason)}
-            {renderInput('Reject Reason', adminRegistrationApplicationDetails.rejectReason)}
-            {renderInput('Institution Name', adminRegistrationApplicationDetails.institution?.name)}
-            {renderInput('User ID', adminRegistrationApplicationDetails.user?.id)}
-            {renderInput('User First Name', adminRegistrationApplicationDetails.user?.firstName)}
-            {renderInput('User Last Name', adminRegistrationApplicationDetails.user?.lastName)}
-            {renderInput('User City', adminRegistrationApplicationDetails.user?.city)}
-            {renderInput('User Email Address', adminRegistrationApplicationDetails.user?.emailAddress)}
-            {renderPhoneInput(adminRegistrationApplicationDetails.user?.phoneNumber?.countryCode ?? '', adminRegistrationApplicationDetails.user?.phoneNumber?.lineNumber ?? '')}
+            {renderInput(
+              'Reject Reason',
+              adminRegistrationApplicationDetails.rejectReason,
+            )}
+            {renderInput(
+              'Institution Name',
+              adminRegistrationApplicationDetails.institution?.name,
+            )}
+            {renderInput(
+              'User ID',
+              adminRegistrationApplicationDetails.user?.id,
+            )}
+            {renderInput(
+              'User First Name',
+              adminRegistrationApplicationDetails.user?.firstName,
+            )}
+            {renderInput(
+              'User Last Name',
+              adminRegistrationApplicationDetails.user?.lastName,
+            )}
+            {renderInput(
+              'User City',
+              adminRegistrationApplicationDetails.user?.city,
+            )}
+            {renderInput(
+              'User Email Address',
+              adminRegistrationApplicationDetails.user?.emailAddress,
+            )}
+            {renderPhoneInput(
+              adminRegistrationApplicationDetails.user?.phoneNumber
+                ?.countryCode ?? '',
+              adminRegistrationApplicationDetails.user?.phoneNumber
+                ?.lineNumber ?? '',
+            )}
           </div>
         </form>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default Page;
+export default Page
