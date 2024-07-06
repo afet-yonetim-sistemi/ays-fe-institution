a057b932477a8adfeb1feef712091221579d5c39
sharp3614 <knurhak09@gmail.com>
2024-07-06 14:55:43 +0300

    added new menu item

--- src/constants/menuItems.ts
+++ src/constants/menuItems.ts
@@ -1,4 +1,4 @@
-import { HomeIcon, UserRoundCheck } from 'lucide-react'
+import { Ambulance, HomeIcon, UserRoundCheck } from 'lucide-react'
 import React from 'react'
 import { Permission } from '@/constants/permissions'
 
@@ -20,5 +20,11 @@ export const MenuItems: Menu[] = [
     label: 'adminRegistrationApplications',
     icon: UserRoundCheck,
     requiredPermissions: [Permission.APPLICATION_LIST],
+  },
+  {
+    key: '/emergency-evacuation-applications',
+    label: 'emergencyEvacuationApplications',
+    icon: Ambulance,
+    requiredPermissions: [Permission.EVACUATION_LIST],
   },
 ]
