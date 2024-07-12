7e1de3589681be7cb063da38418aa25761a21931
sharp3614 <knurhak09@gmail.com>
2024-07-12 20:56:02 +0300

    Added `w-fit` class to `TooltipTrigger` element in `DataTableFilter` component.

--- src/components/dataTable/dataTableFilter.tsx
+++ src/components/dataTable/dataTableFilter.tsx
@@ -37,7 +37,7 @@ export function DataTableFilter<TData, TValue>({
     <DropdownMenu>
       <TooltipProvider>
         <Tooltip>
-          <TooltipTrigger className="flex items-center gap-2 ">
+          <TooltipTrigger className="flex w-fit items-center gap-2 ">
             <DropdownMenuTrigger
               asChild
               className="hover:bg-muted/90 data-[state=open]:bg-blue-600/10 data-[state=open]:text-blue-600 rounded h-10 px-4 py-2"
