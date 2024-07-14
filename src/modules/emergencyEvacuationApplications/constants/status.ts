adfe3794ae46bd189f005d5432de31084f0d2bc1
sharp3614 <knurhak09@gmail.com>
2024-07-14 15:49:09 +0300

    updated: status colors and added 'completed' status in StatusData constants

--- src/modules/emergencyEvacuationApplications/constants/status.ts
+++ src/modules/emergencyEvacuationApplications/constants/status.ts
@@ -1,25 +1,26 @@
 export const StatusData = [
-  { label: 'pending', value: 'PENDING', color: 'bg-yellow-500 text-white' },
-  { label: 'inReview', value: 'IN_REVIEW', color: 'bg-green-500 text-white' },
+  { label: 'pending', value: 'PENDING', color: 'bg-gray-500 text-white' },
+  { label: 'inReview', value: 'IN_REVIEW', color: 'bg-teal-500 text-white' },
   {
     label: 'inProgress',
     value: 'IN_PROGRESS',
-    color: 'bg-red-500 text-white',
+    color: 'bg-yellow-500 text-white',
   },
   {
     label: 'receivedFirstApprove',
     value: 'RECEIVED_FIRST_APPROVE',
-    color: 'bg-sky-500 text-white',
+    color: 'bg-blue-500 text-white',
   },
   {
     label: 'receivedSecondApprove',
     value: 'RECEIVED_SECOND_APPROVE',
-    color: 'bg-gray-500 text-white',
+    color: 'bg-blue-600 text-white',
   },
   {
     label: 'receivedThirdApprove',
     value: 'RECEIVED_THIRD_APPROVE',
-    color: 'bg-gray-500 text-white',
+    color: 'bg-blue-700 text-white',
   },
-  { label: 'canceled', value: 'CANCELLED', color: 'bg-gray-500 text-white' },
+  { label: 'completed', value: 'COMPLETED', color: 'bg-green-500 text-white' },
+  { label: 'canceled', value: 'CANCELLED', color: 'bg-red-500 text-white' },
 ]
