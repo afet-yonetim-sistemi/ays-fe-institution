952f12679adb900c8623974e939d35156c4d92eb
agitrubard <demiragitrubar@gmail.com>
2024-07-24 16:19:29 +0300

    AYS-83 | `formatDate` Function Has Been Upgraded with Dynamic TimeZone and New Format

--- src/lib/formatDate.ts
+++ src/lib/formatDate.ts
@@ -1,6 +1,8 @@
+import { DateTime } from "luxon";
+
 export const formatDate = (date: string) => {
-  return date.replace(
-    /(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):\d{2}/,
-    '$3.$2.$1 $4:$5'
-  )
-}
+  return DateTime
+    .fromISO(date, { zone: "UTC" })
+    .setZone(DateTime.local().zoneName)
+    .toFormat("dd.MM.yyyy HH:mm");
+};
