c9b9270c5f162784066d566186dea6ee59f02915
Buğra Ercan <bugrasshgercan@gmail.com>
2024-08-07 00:03:44 +0300

    AYS-298: (WIP) updated page a little bit, TODO fetch looks broken

--- src/modules/roleListing/components/columns.tsx
+++ src/modules/roleListing/components/columns.tsx
@@ -7,7 +7,9 @@ import { RoleListingTableProps } from '../constants/types';
 export const columns: ColumnDef<RoleListingTableProps>[] = [
   {
     accessorKey: 'name',
-    header: () => i18next.t('roleName'),
+    header: ({ column }) => (
+      <DataTableSort column={column} label={i18next.t('roleName')} />
+    ),
     cell: ({ row }) => row.original.name,
     size: 200,
   },
@@ -19,13 +21,13 @@ export const columns: ColumnDef<RoleListingTableProps>[] = [
   },
   {
     accessorKey: 'createdAt',
-    header: ({ column }) => <DataTableSort column={column} />,
-    cell: ({ row }) => <div className="px-2">{formatDateTime(row.getValue('createdAt'))}</div>,
+    header: () => i18next.t('createdDateTime'),
+    cell: ({ row }) => <div className="px-2">{formatDateTime(row.getValue('updatedAt'))}</div>,
     size: 170,
   },
   {
     accessorKey: 'updatedAt',
-    header: () => i18next.t('updatedAt'),
+    header: () => i18next.t('updatedDateTime'),
     cell: ({ row }) => <div className="px-2">{formatDateTime(row.getValue('updatedAt'))}</div>,
     size: 170,
   },
