a61c3b9fe664a8f9bde0afa465c1a9af26bd08e3
Buğra Ercan <bugrasshgercan@gmail.com>
2024-10-15 13:51:12 +0300

    feat: add - as a special character

--- src/lib/hasNoNumberNoSpecialChar.ts
+++ src/lib/hasNoNumberNoSpecialChar.ts
@@ -1,4 +1,4 @@
 export const hasNoNumberNoSpecialChar = (str: string): boolean => {
-  const strRegex = /^[a-zA-ZçÇğĞıİşŞüÜöÖ\s-]+$/
+  const strRegex = /^[a-zA-ZçÇğĞıİşŞüÜöÖ\s]+$/
   return strRegex.test(str)
 }
