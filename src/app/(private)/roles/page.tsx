dc438395847d55050d771ae942ee188b94121898
Buğra Ercan <bugrasshgercan@gmail.com>
2024-11-22 13:20:39 +0300

    feat: update role listing page to use new filterinput component

--- src/app/(private)/roles/page.tsx
+++ src/app/(private)/roles/page.tsx
@@ -1,8 +1,7 @@
 'use client'
 
 import { DataTable } from '@/components/ui/data-table'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
+import FilterInput from '@/components/ui/filter-input'
 import StatusFilter from '@/components/ui/status-filter'
 import { Toaster } from '@/components/ui/toaster'
 import { StatusData } from '@/constants/statusData'
@@ -10,7 +9,6 @@ import { useHandleFilterChange } from '@/hooks/useHandleFilterChange'
 import { usePagination } from '@/hooks/usePagination'
 import { useSort } from '@/hooks/useSort'
 import { handleApiError } from '@/lib/handleApiError'
-import { cn } from '@/lib/utils'
 import { columns, Role } from '@/modules/roles/components/columns'
 import { RolesFilter } from '@/modules/roles/constants/types'
 import { getRoles } from '@/modules/roles/service'
@@ -106,23 +104,12 @@ const Page = (): JSX.Element => {
           selectedStatuses={filters.statuses}
           onStatusChange={(statuses) => handleFilterChange('status', statuses)}
         />
-        <div className="relative">
-          <Input
-            id="roleName"
-            placeholder=""
-            value={filters.name}
-            onChange={(e) => handleFilterChange('name', e.target.value)}
-            className={cn(
-              'block focus-visible:ring-0 focus-visible:ring-offset-0 p-3 w-full text-sm text-gray-900 bg-transparent rounded-lg border-[2px] border-gray-200 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
-            )}
-          />
-          <Label
-            htmlFor="roleName"
-            className="absolute !left-3 rounded cursor-text text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 top-1.5 z-10 origin-[0] bg-white dark:bg-background peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-1.5 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
-          >
-            {t('role.name')}
-          </Label>
-        </div>
+        <FilterInput
+          id="name"
+          label={t('name')}
+          value={filters.name}
+          onChange={(e) => handleFilterChange('name', e.target.value)}
+        />
       </div>
       <DataTable
         columns={columns({ sort: filters.sort }, handleSortChange)}
