ca6997eabb621e13d551e1e5975e1281f4f826d1
Buğra Ercan <bugrasshgercan@gmail.com>
2024-11-25 01:20:41 +0300

    fix: emailaddres to emailadress

--- src/modules/adminRegistrationApplications/constants/formValidationSchema.ts
+++ src/modules/adminRegistrationApplications/constants/formValidationSchema.ts
@@ -63,7 +63,7 @@ export const InstitutionFormSchema = z.object({
   emailAddress: z
     .string({
       required_error: i18n.t('requiredField', {
-        field: i18n.t('emailAddres'),
+        field: i18n.t('emailAddress'),
       }),
     })
     .regex(emailRegex, i18n.t('invalidEmail')),
--- src/modules/login/constants/formValidationSchema.ts
+++ src/modules/login/constants/formValidationSchema.ts
@@ -6,7 +6,7 @@ const LoginFormSchema = z.object({
   emailAddress: z
     .string({
       required_error: i18n.t('requiredField', {
-        field: i18n.t('emailAddres'),
+        field: i18n.t('emailAddress'),
       }),
     })
     .regex(emailRegex, i18n.t('invalidEmail')),
