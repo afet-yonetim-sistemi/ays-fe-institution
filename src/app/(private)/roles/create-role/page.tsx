579d1976f3179fa3d7d3940bde16a092ef7b0dde
Buğra Ercan <bugrasshgercan@gmail.com>
2024-12-10 23:17:26 +0300

    feat: create initial create role page

---
+++ src/app/(private)/roles/create-role/page.tsx
@@ -0,0 +1,25 @@
+// 'use client'
+
+// import { useToast } from '@/components/ui/use-toast'
+// import { CreateRoleSchema } from '@/modules/roles/constants/formValidationSchema'
+// import { zodResolver } from '@hookform/resolvers/zod'
+// import { useRouter } from 'next/navigation'
+// import { useState } from 'react'
+// import { useForm } from 'react-hook-form'
+// import { useTranslation } from 'react-i18next'
+
+// const Page = (): JSX.Element => {
+//   const { t } = useTranslation()
+//   const { toast } = useToast()
+//   const router = useRouter()
+//   const [isLoading, setIsLoading] = useState(true)
+//   const form = useForm({
+//     resolver: zodResolver(CreateRoleSchema),
+//     mode: 'onChange',
+//   })
+//   const { control, reset, formState } = form
+
+//   return <div></div>
+// }
+
+// export default Page
