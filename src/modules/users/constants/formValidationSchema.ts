0680bad27b8e12dc75ac293de7aa8661841cbf42
Buğra Ercan <bugrasshgercan@gmail.com>
2024-12-17 11:49:10 +0300

    feat: fix formvalidation phone number

--- src/modules/users/constants/formValidationSchema.ts
+++ src/modules/users/constants/formValidationSchema.ts
@@ -1,42 +1,41 @@
+import { PhoneNumberSchema } from '@/constants/formValidationSchema'
 import { emailRegex } from '@/constants/regex'
-import i18n from '@/i18n'
 import { nameboxWithLengthValidation } from '@/lib/nameboxValidation'
+import { t } from 'i18next'
 import { z } from 'zod'
 
-const PhoneNumberSchema = z
-  .object({
-    countryCode: z.string(),
-    lineNumber: z.string(),
-  })
-  .refine((phoneNumber) => phoneNumber.countryCode && phoneNumber.lineNumber, {
-    message: i18n.t('invalidPhoneNumber'),
-  })
+const PhoneNumberSchemaWithRefine = PhoneNumberSchema.refine(
+  (phoneNumber) => phoneNumber.countryCode && phoneNumber.lineNumber,
+  {
+    message: t('invalidPhoneNumber'),
+  }
+)
 
-  export const UserValidationSchema = z.object({
-    id: z.string(),
-    firstName: z.string(),
-    lastName: z.string(),
-    phoneNumber: PhoneNumberSchema,
-    city: z.string(),
-    status: z.string(),
-    createdUser: z.string(),
-    createdAt: z.string(),
-    updatedUser: z.string(),
-    updatedAt: z.string(),
-  })
+export const UserValidationSchema = z.object({
+  id: z.string(),
+  firstName: z.string(),
+  lastName: z.string(),
+  phoneNumber: PhoneNumberSchemaWithRefine,
+  city: z.string(),
+  status: z.string(),
+  createdUser: z.string(),
+  createdAt: z.string(),
+  updatedUser: z.string(),
+  updatedAt: z.string(),
+})
 
 export const CreateUserValidationSchema = z.object({
   firstName: nameboxWithLengthValidation('firstName', 2, 100),
   lastName: nameboxWithLengthValidation('lastName', 2, 100),
   emailAddress: z
     .string({
-      required_error: i18n.t('requiredField', {
-        field: i18n.t('emailAddress'),
+      required_error: t('requiredField', {
+        field: t('emailAddress'),
       }),
     })
-    .regex(emailRegex, i18n.t('invalidEmail')),
-  phoneNumber: PhoneNumberSchema,
+    .regex(emailRegex, t('invalidEmail')),
+  phoneNumber: PhoneNumberSchemaWithRefine,
   city: z.string({
-    required_error: i18n.t('requiredField', { field: i18n.t('city') }),
+    required_error: t('requiredField', { field: t('city') }),
   }),
 })
