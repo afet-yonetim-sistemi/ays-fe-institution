3214e4b1d20cb618a6070eadcc50d7a5ac4a08ad
Buğra Ercan <38170967+sanshigo345@users.noreply.github.com>
2024-12-23 17:08:36 +0300

    AYS-584 | add wait time before fetching in listing pages (#274)

--- src/app/(private)/roles/page.tsx
+++ src/app/(private)/roles/page.tsx
@@ -23,7 +23,7 @@ import { useAppSelector } from '@/store/hooks'
 import { debounce } from 'lodash'
 import Link from 'next/link'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 const Page = (): JSX.Element => {
@@ -60,7 +60,7 @@ const Page = (): JSX.Element => {
           const { content, totalElementCount, totalPageCount } =
             response.data.response
 
-          if (filters.page > totalPageCount && totalPageCount != 0) {
+          if (filters.page > totalPageCount && totalPageCount !== 0) {
             router.push('/not-found')
             return
           }
@@ -95,31 +95,36 @@ const Page = (): JSX.Element => {
       sort: column ? [{ column, direction: direction as SortDirection }] : [],
     }
     setFilters(updatedFilters)
-
-    const result = getStringFilterValidation().safeParse(updatedFilters.name)
-    if (name && !result.success) {
-      toast({
-        title: t('common.error'),
-        description: t('filterValidation'),
-        variant: 'destructive',
-      })
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchParams, pageSize])
 
   useEffect(() => {
     syncFiltersWithQuery()
   }, [syncFiltersWithQuery])
 
+  const debouncedFetchData = useMemo(
+    () =>
+      debounce((filters: RolesFilter) => {
+        const result = getStringFilterValidation().safeParse(filters.name)
+        if (filters.name && !result.success) {
+          toast({
+            title: t('common.error'),
+            description: t('filterValidation'),
+            variant: 'destructive',
+          })
+          return
+        }
+        fetchData(filters)
+      }, 500),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [fetchData]
+  )
+
   useEffect(() => {
-    const debouncedFetchData = debounce((filters: RolesFilter) => {
-      fetchData(filters)
-    }, 500)
     debouncedFetchData(filters)
     return () => {
       debouncedFetchData.cancel()
     }
-  }, [filters, fetchData])
+  }, [filters, debouncedFetchData])
 
   return (
     <div className="space-y-4">
