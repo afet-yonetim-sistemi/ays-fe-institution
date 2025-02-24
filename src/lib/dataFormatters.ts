1658974593d0bff5b7e88c8892041b5faae5c81f
Buğra Ercan <bugrasshgercan@gmail.com>
2025-02-24 16:10:40 +0300

    refactor: collect formatter functions in one file

---
+++ src/lib/dataFormatters.ts
@@ -0,0 +1,24 @@
+import { DateTime } from 'luxon'
+import { PhoneNumber } from '@/common/types'
+
+export const formatDateTime = (date: string | null): string => {
+  if (!date) return ''
+  return DateTime.fromISO(date, { zone: 'UTC' })
+    .setZone(DateTime.local().zoneName)
+    .toFormat('dd.MM.yyyy HH:mm')
+}
+
+export const formatPhoneNumber = (phoneNumber: PhoneNumber | null): string => {
+  if (!phoneNumber) return ''
+  const { countryCode = '', lineNumber } = phoneNumber
+  const fullNumber = countryCode + lineNumber
+  return fullNumber.replace(
+    /(\d{2})(\d{3})(\d{3})(\d{2})(\d{2})/,
+    '+$1 ($2) $3 $4 $5'
+  )
+}
+
+export const formatReferenceNumber = (number: string | null): string => {
+  if (!number) return ''
+  return number.replace(/(\d)(?=(\d{3})+$)/g, '$1 ')
+}
--- src/lib/formatReferenceNumber.ts
+++
@@ -1,4 +0,0 @@
-export const formatReferenceNumber = (number?: string | null): string => {
-  if (!number) return ''
-  return number.replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
-}
