0d4e4c61ef49dee9f19184d283f16f2b51090ae0
Buğra Ercan <bugrasshgercan@gmail.com>
2025-02-27 17:54:29 +0300

    feat: remove navbar from layout to add navbar individually

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,15 +1,8 @@
-import LanguageToggle from '@/components/dashboard/languageToggle'
-import { ModeToggle } from '@/components/dashboard/modeToggle'
-
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>): JSX.Element {
   return (
     <div className="overflow-auto scrollbar-gutter-stable h-screen">
-      <nav className="fixed flex gap-2 right-8 top-4">
-        <ModeToggle />
-        <LanguageToggle />
-      </nav>
       {children}
     </div>
   )
