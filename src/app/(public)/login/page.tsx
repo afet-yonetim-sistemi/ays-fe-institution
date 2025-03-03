33ebc2aa94bc0c39a499a44ac5a412c31f3730bb
Buğra Ercan <bugrasshgercan@gmail.com>
2025-03-03 18:33:29 +0300

    feat: fix translation bug for login page

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -21,16 +21,11 @@ import {
 import { Input } from '@/components/ui/input'
 import { LoadingSpinner } from '@/components/ui/loadingSpinner'
 import { PasswordInput } from '@/components/ui/passwordInput'
-import { Toaster } from '@/components/ui/toaster'
 import { handleApiError } from '@/lib/handleApiError'
-import {
-  loginFailed,
-  loginSuccess,
-  selectError,
-} from '@/modules/auth/authSlice'
+import { loginFailed, loginSuccess } from '@/modules/auth/authSlice'
 import authService from '@/modules/auth/service'
 import { FormValidationSchema } from '@/modules/login/constants/formValidationSchema'
-import { useAppDispatch, useAppSelector } from '@/store/hooks'
+import { useAppDispatch } from '@/store/hooks'
 import { zodResolver } from '@hookform/resolvers/zod'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
@@ -43,7 +38,6 @@ const Page = (): JSX.Element => {
   const { t } = useTranslation()
   const dispatch = useAppDispatch()
   const router = useRouter()
-  const error = useAppSelector(selectError)
 
   const [loading, setLoading] = useState(false)
 
@@ -68,78 +62,72 @@ const Page = (): JSX.Element => {
         dispatch(loginFailed(error.message))
         form.setValue('password', '')
         handleApiError(error, {
-          description: t('error.invalidEmailOrPassword'),
+          description: 'error.invalidEmailOrPassword',
         })
       })
       .finally(() => setLoading(false))
   }
 
   return (
-    <>
-      {error && <Toaster />}
-      <div className={'container'}>
-        <nav className="fixed flex gap-2 right-8 top-4">
-          <ModeToggle />
-          <LanguageToggle />
-        </nav>
-        <Card className={'w-[410px] h-fit'}>
-          <CardHeader className={'flex items-center'}>
-            <Image
-              src={'/aysfavicon360.png'}
-              alt={'AYS'}
-              width={100}
-              height={100}
-            />
-            <CardTitle>{t('welcome')}</CardTitle>
-            <CardDescription>{t('loginDescription')}</CardDescription>
-          </CardHeader>
-          <CardHeader>
-            <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="space-y-5"
-              >
-                <FormField
-                  control={form.control}
-                  name="emailAddress"
-                  disabled={loading}
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>{t('email')}</FormLabel>
-                      <FormControl>
-                        <Input placeholder={t('email')} {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="password"
-                  disabled={loading}
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>{t('password')}</FormLabel>
-                      <FormControl>
-                        <PasswordInput placeholder={t('password')} {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <Button type="submit" disabled={loading} className={'w-full'}>
-                  {loading ? <LoadingSpinner /> : t('login')}
-                </Button>
-                <ForgotPasswordModal
-                  disabled={loading}
-                  loginEmail={form.watch('emailAddress')}
-                />
-              </form>
-            </Form>
-          </CardHeader>
-        </Card>
-      </div>
-    </>
+    <div className={'container'}>
+      <nav className="fixed flex gap-2 right-8 top-4">
+        <ModeToggle />
+        <LanguageToggle />
+      </nav>
+      <Card className={'w-[410px] h-fit'}>
+        <CardHeader className={'flex items-center'}>
+          <Image
+            src={'/aysfavicon360.png'}
+            alt={'AYS'}
+            width={100}
+            height={100}
+          />
+          <CardTitle>{t('welcome')}</CardTitle>
+          <CardDescription>{t('loginDescription')}</CardDescription>
+        </CardHeader>
+        <CardHeader>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
+              <FormField
+                control={form.control}
+                name="emailAddress"
+                disabled={loading}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t('email')}</FormLabel>
+                    <FormControl>
+                      <Input placeholder={t('email')} {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                disabled={loading}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{t('password')}</FormLabel>
+                    <FormControl>
+                      <PasswordInput placeholder={t('password')} {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" disabled={loading} className={'w-full'}>
+                {loading ? <LoadingSpinner /> : t('login')}
+              </Button>
+              <ForgotPasswordModal
+                disabled={loading}
+                loginEmail={form.watch('emailAddress')}
+              />
+            </form>
+          </Form>
+        </CardHeader>
+      </Card>
+    </div>
   )
 }
 
