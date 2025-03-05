a5dd19ce58335fa639f4c3ec04b6738ec726a379
Buğra Ercan <bugrasshgercan@gmail.com>
2025-03-05 23:23:48 +0300

    feat: add status object

--- src/modules/emergencyEvacuationApplications/constants/statuses.ts
+++ src/modules/emergencyEvacuationApplications/constants/statuses.ts
@@ -1,20 +1,28 @@
 import { LabeledItem } from '@/common/types'
 
 export const emergencyEvacuationApplicationStatuses: LabeledItem[] = [
-  { label: 'pending', value: 'PENDING', color: 'bg-gray-500 text-white' },
-  { label: 'inReview', value: 'IN_REVIEW', color: 'bg-teal-500 text-white' },
   {
-    label: 'receivedFirstApprove',
+    label: 'status.pending',
+    value: 'PENDING',
+    color: 'bg-gray-500 text-white',
+  },
+  {
+    label: 'status.inReview',
+    value: 'IN_REVIEW',
+    color: 'bg-teal-500 text-white',
+  },
+  {
+    label: 'status.receivedFirstApprove',
     value: 'RECEIVED_FIRST_APPROVE',
     color: 'bg-blue-500 text-white',
   },
   {
-    label: 'receivedSecondApprove',
+    label: 'status.receivedSecondApprove',
     value: 'RECEIVED_SECOND_APPROVE',
     color: 'bg-blue-600 text-white',
   },
   {
-    label: 'receivedThirdApprove',
+    label: 'status.receivedThirdApprove',
     value: 'RECEIVED_THIRD_APPROVE',
     color: 'bg-blue-700 text-white',
   },
@@ -23,5 +31,9 @@ export const emergencyEvacuationApplicationStatuses: LabeledItem[] = [
     value: 'COMPLETED',
     color: 'bg-green-500 text-white',
   },
-  { label: 'cancelled', value: 'CANCELLED', color: 'bg-red-500 text-white' },
+  {
+    label: 'status.cancelled',
+    value: 'CANCELLED',
+    color: 'bg-red-500 text-white',
+  },
 ]
--- src/modules/roles/constants/statuses.ts
+++ src/modules/roles/constants/statuses.ts
@@ -1,7 +1,11 @@
 import { LabeledItem } from '@/common/types'
 
 export const roleStatuses: LabeledItem[] = [
-  { label: 'active', value: 'ACTIVE', color: 'bg-green-500 text-white' },
-  { label: 'passive', value: 'PASSIVE', color: 'bg-yellow-500 text-white' },
-  { label: 'deleted', value: 'DELETED', color: 'bg-red-500 text-white' },
+  { label: 'status.active', value: 'ACTIVE', color: 'bg-green-500 text-white' },
+  {
+    label: 'status.passive',
+    value: 'PASSIVE',
+    color: 'bg-yellow-500 text-white',
+  },
+  { label: 'status.deleted', value: 'DELETED', color: 'bg-red-500 text-white' },
 ]
