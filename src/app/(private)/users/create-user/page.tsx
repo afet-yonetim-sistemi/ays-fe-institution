af222771bd008e5054ec9fac40216d21bc6a1b84
Buğra Ercan <bugrasshgercan@gmail.com>
2025-03-11 21:00:45 +0300

    feat: add new phone component to create user page

--- src/app/(private)/users/create-user/page.tsx
+++ src/app/(private)/users/create-user/page.tsx
@@ -11,6 +11,7 @@ import {
   FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import PhoneInput from '@/components/ui/phone-input'
 import { Switch } from '@/components/ui/switch'
 import useFetchRoleSummary from '@/hooks/useFetchRoleSummary'
 import { useToast } from '@/hooks/useToast'
@@ -22,6 +23,7 @@ import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
+import { CountryData } from 'react-phone-input-2'
 
 const Page = (): JSX.Element => {
   const { t } = useTranslation()
@@ -142,7 +144,7 @@ const Page = (): JSX.Element => {
                 </FormItem>
               )}
             />
-            {/* <FormField
+            <FormField
               control={control}
               name="phoneNumber"
               render={({ field }) => (
@@ -150,14 +152,23 @@ const Page = (): JSX.Element => {
                   <FormLabel>{t('common.phoneNumber')}</FormLabel>
                   <FormControl>
                     <PhoneInput
-                      onChange={field.onChange}
-                      disableCountrySelection={true}
+                      value={
+                        (field.value?.countryCode || '') +
+                        (field.value?.lineNumber || '')
+                      }
+                      onChange={(value: string, country: CountryData) => {
+                        const countryCode: string = country.dialCode
+                        const lineNumber: string = value.slice(
+                          countryCode.length
+                        )
+                        field.onChange({ countryCode, lineNumber })
+                      }}
                     />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
-            /> */}
+            />
             <FormField
               control={control}
               name="city"
