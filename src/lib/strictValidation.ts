ae079bbc58363284b936caf1ef32a86f2015dd95
Buğra Ercan <bugrasshgercan@gmail.com>
2025-07-13 18:39:41 +0300

    fix: fix sonarqube issue again

--- src/lib/strictValidation.ts
+++ src/lib/strictValidation.ts
@@ -23,12 +23,12 @@ export const strictNameValidation = z
   .string()
   .min(2, { message: 'validation.minLength' })
   .max(100, { message: 'validation.maxLength' })
-  .regex(/^\p{L}(?:[\p{L}\s.,'\-]*\p{L})?$/u, {
+  .regex(/^\p{L}(?:[\p{L}\s.,'-]*\p{L})?$/u, {
     message: 'validation.invalid',
   })
   .refine((val) => !/ {2,}/.test(val), {
     message: 'validation.invalid',
   })
-  .refine((val) => !/[.,'\-]{2,}/.test(val), {
+  .refine((val) => !/[.,'-]{2,}/.test(val), {
     message: 'validation.invalid',
   })
