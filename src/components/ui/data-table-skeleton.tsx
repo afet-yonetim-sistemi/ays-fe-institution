90532ab94e16482d99a533839f125528ea54e01b
feyzanursener <senerffeyza@gmail.com>
2025-11-06 19:35:42 +0300

    AYS-923 | removed `useId` hook and keys updated

--- src/components/ui/data-table-skeleton.tsx
+++ src/components/ui/data-table-skeleton.tsx
@@ -3,9 +3,6 @@
 import { ColumnDef } from '@tanstack/react-table'
 import { TableRow, TableCell } from '@/components/ui/table'
 import { Skeleton } from './skeleton'
-import { useId } from 'react'
-
-
 
 interface DataTableSkeletonProps<TData extends { id: string }, TValue> {
   columns: ColumnDef<TData, TValue>[]
@@ -16,30 +13,19 @@ export function DataTableSkeleton<TData extends { id: string }, TValue>({
   columns,
   pageSize,
 }: Readonly<DataTableSkeletonProps<TData, TValue>>): JSX.Element {
-
-  const baseId = useId()
-
   return (
-   
-  <>
-  
-  {Array.from({ length: pageSize }, (_, rowIndex) => (
-    
-    <TableRow 
-    key={`${baseId}-row-${rowIndex}`} 
-    aria-hidden="true"
-    
-    >
-      {columns.map((_, colIndex) => {
-        return (
-          <TableCell key={`${baseId}-col-${colIndex}`}>
-            <Skeleton className="h-3 md:h-4 w-full" />
-          </TableCell>
-        )
-      })}
-    </TableRow>
-  ))}
-</>
-
+    <>
+      {Array.from({ length: pageSize }, (_, rowIndex) => (
+        <TableRow key={`skeleton-row-${rowIndex}`} aria-hidden="true">
+          {columns.map((_, colIndex) => {
+            return (
+              <TableCell key={`skeleton-row-${rowIndex}-col-${colIndex}`}>
+                <Skeleton className="h-3 md:h-4 w-full" />
+              </TableCell>
+            )
+          })}
+        </TableRow>
+      ))}
+    </>
   )
 }
