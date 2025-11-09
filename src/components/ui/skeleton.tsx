081f8d0a74fd2630a86bdd8269549c5c9063e5c0
feyzanursener <senerffeyza@gmail.com>
2025-11-09 21:20:15 +0300

    AYS-923 | Fixed a sonarqube bug in the `skeleton.tsx` file

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,12 +1,12 @@
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils'
 
 function Skeleton({
   className,
   ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
+}: Readonly<React.HTMLAttributes<HTMLDivElement>>) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-muted", className)}
+      className={cn('animate-pulse rounded-md bg-muted', className)}
       {...props}
     />
   )
