eaadcb6f9fecf7a7b11b5b2ce83df97ebf1d06b3
FurkanOrak <50626097+FurkanOrak@users.noreply.github.com>
2025-12-22 03:13:05 +0300

    AYS-957 | Implement UserInfoCard component and integrate it into the dashboard

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,35 +1,13 @@
 'use client'
 
-import { Card, CardContent } from '@/components/ui/card'
-import { formatDateTime } from '@/lib/dataFormatters'
+import UserInfoCard from '@/components/dashboard/UserInfoCard'
 import { selectUser } from '@/modules/auth/authSlice'
 import { useAppSelector } from '@/store/hooks'
-import { CalendarCheck, Landmark, User } from 'lucide-react'
 import { useTranslation } from 'react-i18next'
 
 const Page = (): JSX.Element => {
   const { t } = useTranslation()
   const user = useAppSelector(selectUser)
-
-  const infoItems = user
-    ? [
-        {
-          Icon: User,
-          label: 'common.fullName',
-          value: `${user.userFirstName} ${user.userLastName}`,
-        },
-        {
-          Icon: Landmark,
-          label: 'common.institution',
-          value: user.institutionName,
-        },
-        {
-          Icon: CalendarCheck,
-          label: 'common.lastSuccessfulLoginAt',
-          value: formatDateTime(user.userLastLoginAt),
-        },
-      ]
-    : []
 
   return (
     <div className="space-y-4">
@@ -41,23 +19,7 @@ const Page = (): JSX.Element => {
           {t('dashboard.description')}
         </p>
       </div>
-      {user && (
-        <Card className="mb-6">
-          <CardContent className="p-6">
-            <div className="flex flex-col gap-3">
-              {infoItems.map(({ Icon, label, value }) => (
-                <div className="flex gap-1.5" key={label}>
-                  <span>
-                    <Icon />
-                  </span>
-                  <span className="font-semibold">{t(label)}:</span>
-                  <span>{value}</span>
-                </div>
-              ))}
-            </div>
-          </CardContent>
-        </Card>
-      )}
+      <UserInfoCard userInfo={user}/>
     </div>
   )
 }
