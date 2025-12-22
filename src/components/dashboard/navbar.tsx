7c1bf6465083c43f5d6d5b8cac946454ce414539
Abdullah KUŞ <abdullahkuscom@gmail.com>
2025-12-22 20:22:02 +0300

    AYS-942 | The Navigation Bar Has Been Made Responsive And The Use Of User Info Has Been Changed

--- src/components/dashboard/navbar.tsx
+++ src/components/dashboard/navbar.tsx
@@ -6,14 +6,16 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
-import { parseJwt } from '@/lib/helpers'
 import { showErrorToast } from '@/lib/showToast'
-import { selectRefreshToken, selectToken } from '@/modules/auth/authSlice'
+import { selectRefreshToken, selectUser } from '@/modules/auth/authSlice'
+import type { UserInfo } from '@/modules/auth/constants/types'
 import authService from '@/modules/auth/service'
 import { useAppDispatch, useAppSelector } from '@/store/hooks'
 import { MenuIcon } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
+import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { LuUser } from 'react-icons/lu'
 import { Avatar, AvatarFallback } from '../ui/avatar'
@@ -23,13 +25,31 @@ import LanguageToggle from './languageToggle'
 import Menu from './menu'
 import { ModeToggle } from './modeToggle'
 
+interface UserInfoProps {
+  userInfo?: UserInfo | null
+  className?: string
+}
+
+const UserInfoDisplay: React.FC<UserInfoProps> = ({ userInfo, className }) => {
+  const isDropdown = className === 'grid'
+  return (
+    <div className={className || 'hidden sm:grid'}>
+      <span
+        className={`text-sm font-bold ${isDropdown ? 'text-left' : 'flex justify-end'}`}
+      >
+        {userInfo?.userFirstName} {userInfo?.userLastName}
+      </span>
+      <span className={'text-sm'}>{userInfo?.institutionName}</span>
+    </div>
+  )
+}
+
 function Navbar(): JSX.Element {
   const { t } = useTranslation()
-  const token = useAppSelector(selectToken)
+  const userInfo = useAppSelector(selectUser)
   const refreshToken = useAppSelector(selectRefreshToken)
   const dispatch = useAppDispatch()
   const router = useRouter()
-  const userInfo = parseJwt(token)
 
   const logout = (): void => {
     authService
@@ -42,6 +62,7 @@ function Navbar(): JSX.Element {
         showErrorToast(error)
       })
   }
+
   return (
     <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
       <Sheet>
@@ -58,29 +79,28 @@ function Navbar(): JSX.Element {
           <Menu />
         </SheetContent>
       </Sheet>
-      <div className="flex w-full flex-1 items-center space-x-2">
-        <Image
-          src="/aysLogo40px.svg"
-          alt="AYS Logo"
-          width={40}
-          height={40}
-          priority
-          unoptimized
-        />
-        <div className="text-center md:text-left">{t('common.AYS')}</div>
-      </div>
+      <Link href={'/'}>
+        <div className="flex w-full flex-1 items-center space-x-2">
+          <Image
+            src="/aysLogo40px.svg"
+            alt="AYS Logo"
+            width={40}
+            height={40}
+            priority
+            unoptimized
+          />
+          <div className="hidden text-center md:block md:text-left">
+            {t('common.AYS')}
+          </div>
+        </div>
+      </Link>
       <div className="flex space-x-2">
         <LanguageToggle />
         <ModeToggle />
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <div className={'flex cursor-pointer'}>
-              <div className={'ml-3 grid'}>
-                <span className={'flex justify-end text-sm font-bold'}>
-                  {userInfo?.userFirstName} {userInfo?.userLastName}
-                </span>
-                <span className={'text-sm'}>{userInfo?.institutionName}</span>
-              </div>
+              <UserInfoDisplay userInfo={userInfo} />
               <Avatar className={'float-right ml-3'}>
                 <AvatarFallback>
                   <LuUser size={22} />
@@ -89,6 +109,9 @@ function Navbar(): JSX.Element {
             </div>
           </DropdownMenuTrigger>
           <DropdownMenuContent className="w-56">
+            <DropdownMenuItem disabled className={'block sm:hidden'}>
+              <UserInfoDisplay userInfo={userInfo} className="grid" />
+            </DropdownMenuItem>
             <DropdownMenuItem onClick={() => logout()}>
               {t('navBar.logout')}
             </DropdownMenuItem>
